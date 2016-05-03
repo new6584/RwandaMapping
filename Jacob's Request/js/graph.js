cb0775b305d6601ffc47e3dea28ceb0348073e6e
Aurko-M <aurkomallick@gmail.com>
2016-05-03 15:16:35 -0400

    Added files via upload

--- Jacob's Request/js/graph.js
+++ Jacob's Request/js/graph.js
@@ -11,11 +11,9 @@ function drawChart(dataTable) {
 var data = google.visualization.arrayToDataTable(dataTable);
 
 var options = {
-width: 430,
-  height: 250,
-  title: 'Businesses by Quarter',
-  colors: ['#800080', '#8c198c', '#993299', '#a64ca6', '#b266b2', '#bf7fbf', '#cc99cc'],
-  backgroundColor: '#cccccc'
+	title: 'Businesses by Quarter',
+	colors: ['#800080', '#8c198c', '#993299', '#a64ca6', '#b266b2', '#bf7fbf', '#cc99cc'],
+	backgroundColor: '#cccccc'
 };
 
 var chart = new google.visualization.PieChart(document.getElementById('piechart'));
